2ae4e5d074982f277c04999e31fd2fd85e4d443d
Andrew Hill <androohill@gmail.com>
2012-10-06 00:05:41 +0100

    v0.1

--- pigs.js
+++ pigs.js
@@ -3,45 +3,157 @@ var PIGS = PIGS || {};
 
 PIGS.Odds = {
 	positions: {
-		side: 	{ odds: 0.694, pc: 0.694 },
-		back: 	{ odss: 0.217, pc: 0.911 },
-		stand: 	{ odds: 0.063, pc: 0.974 },
-		snout:	{ odds: 0.017, pc: 0.991 },
-		jowl: 	{ odds: 0.009, pc: 1.000 },
-		bacon:  { odds: 0.010, pc: 0.000 }
+		sidenodot: { odds: 0.349, pc: 0.349, name: 'Side' },
+        sidedot:   { odds: 0.302, pc: 0.651, name: 'Side .' },
+		back: 	    { odss: 0.224, pc: 0.875, name: 'Razorback' },
+		stand: 	    { odds: 0.088, pc: 0.963, name: 'Trotter' },
+		snout:	    { odds: 0.030, pc: 0.993, name: 'Snouter' },
+		jowl:   	{ odds: 0.007, pc: 1.000, name: 'Leaning Jowler' },
+		special:    { odds: 0.050, pc: 0.000, name: 'Special' }
 	},
+    
+    specials: {
+        makinbacon:     { odds: 0.950, pc: 0.950, name: 'Makin Bacon', score: 0 },
+        kissingbacon:   { odds: 0.045, pc: 0.995, name: 'Kissing Bacon', score: 100 },
+        piggyback:      { odds: 0.005, pc: 1.000, name: 'Piggyback', score: 'DQ' }
+    }
 }
 
 PIGS.Score = {
-	
+	1: { pos1:  'sidenodot',   pos2: 'sidenodot', score: { points: 1, name: 'sider' } },
+    2: { pos1:  'sidenodot',   pos2: 'sidedot',   score: { points: 0, name: 'pig out' } },
+    3: { pos1:  'sidenodot',   pos2: 'stand',      score: { points: 5, name: 'mixed combo'} },
+    4: { pos1:  'sidenodot',   pos2: 'back',       score: { points: 5, name: 'mixed combo' } },
+    5: { pos1:  'sidenodot',   pos2: 'snout',      score: { points: 10, name: 'mixed combo' } },
+    6: { pos1:  'sidenodot',   pos2: 'jowl',       score: { points: 15, name: 'mixed combo' } },
+    7: { pos1:  'sidedot',     pos2: 'sidedot',   score: { points: 1, name: 'sider' } },
+    8: { pos1:  'sidedot',     pos2: 'stand',      score: { points: 5, name: 'mixed combo' } },
+    9: { pos1:  'sidedot',     pos2: 'back',       score: { points: 5, name: 'mixed combo' } },
+    10: { pos1: 'sidedot',     pos2: 'snout',      score: { points: 10, name: 'mixed combo' } },
+    11: { pos1: 'sidedot',     pos2: 'jowl',       score: { points: 15, name: 'mixed combo' } },
+    12: { pos1: 'back',         pos2: 'back',       score: { points: 20, name: 'double back' } },
+    13: { pos1: 'back',         pos2: 'stand',      score: { points: 10, name: 'mixed combo' } },
+    14: { pos1: 'back',         pos2: 'snout',      score: { points: 15, name: 'mixed combo' } },
+    15: { pos1: 'back',         pos2: 'jowl',       score: { points: 20, name: 'mixed combo' } },    
+    16: { pos1: 'stand',        pos2: 'stand',      score: { points: 20, name: 'double stand' } },
+    17: { pos1: 'stand',        pos2: 'snout',      score: { points: 15, name: 'mixed combo' } },
+    18: { pos1: 'stand',        pos2: 'jowl',       score: { points: 20, name: 'mixed combo' } },
+    19: { pos1: 'snout',        pos2: 'snout',      score: { points: 40, name: 'double snout' } },
+    20: { pos1: 'snout',        pos2: 'jowl',       score: { points: 25, name: 'mixed combo' } },
+    21: { pos1: 'jowl',         pos2: 'jowl',       score: { points: 60, name: 'double jowl' } },
 }
 
+
+
 PIGS.Actions = {
+    
 	roll: function(player) {
-		var rnd = Math.random().toFixed(3);
-		console.log(rnd);
-		
-		var pos = this.getPosition(rnd);
+        var score = {};
+        var isSpecial = false;
+		var rnd1 = Math.random().toFixed(3);
+        var rnd2 = Math.random().toFixed(3);
+        
+        console.log(rnd1 + ' ' + rnd2);
+        
+        if(rnd1 <= rnd2) {
+            var pos1 = this.getPosition(rnd1);
+            var pos2 = this.getPosition(rnd2);    
+        } else {
+            var pos1 = this.getPosition(rnd2);
+            var pos2 = this.getPosition(rnd1);
+        }
+        
+        score = this.getScore(pos1, pos2);
+        
+        var isSpecial = this.checkSpecial(rnd1);
+        
+        if(isSpecial){
+            pos1 = 'Special';
+            pos2 = this.getSpecialPosition(rnd2);
+            score.points = PIGS.Odds.specials[pos2].score;
+            score.name = PIGS.Odds.specials[pos2].name;
+        }
+        
+        console.log(score);
+    
+        PIGS.UI.showPositions(pos1, pos2);
+        PIGS.UI.showScore(score.points, score.name);
 	},
 	
 	getPosition: function(rnd) {
 		var pos = {};
 	
 		for(idx in PIGS.Odds.positions) {
-			console.log(PIGS.Odds.positions[idx].pc);
+			//console.log(PIGS.Odds.positions[idx].pc);
 		
-			if(rnd < PIGS.Odds.positions[idx].pc) {
+			if(rnd <= PIGS.Odds.positions[idx].pc) {
 				pos = idx;
 				break;
 			}
 		}
 		
-		console.log(pos);
+		return pos;
 	},
+    
+    getSpecialPosition: function(rnd){
+        var pos = {};
+	
+		for(idx in PIGS.Odds.specials) {
+			//console.log(PIGS.Odds.specials[idx].pc);
+		
+			if(rnd <= PIGS.Odds.specials[idx].pc) {
+				pos = idx;
+				break;
+			}
+		}
+		
+		return pos;
+    },
+    
+    getScore: function(pos1, pos2){
+        console.log(pos1 + ' ' + pos2);
+        
+        for(idx in PIGS.Score) {
+            if(PIGS.Score[idx].pos1 == pos1 && PIGS.Score[idx].pos2 == pos2) {
+                return PIGS.Score[idx].score;
+            }
+        }  
+    },
+    
+    checkSpecial: function(rnd){
+        if(rnd <= PIGS.Odds.positions['special'].odds){
+            console.log('Special: ' + rnd);
+            return true;
+        }
+        
+        console.log('Not Special: ' +rnd);
+        return false;
+    }
+}
+
+PIGS.UI = {    
+    showPositions: function(pos1, pos2){
+        
+        $("#pig1-pos").text(pos1);
+        $("#pig2-pos").text(pos2);
+    },
+    
+    showScore: function(points, name) {
+        $("#score").hide();
+        
+        $("#roll-points").text(points);
+        $("#roll-name").text(name);
+        
+        $("#score").show();
+    }
 }
 
 $(function(){
 	$("#roll-pigs").click(function(){
 		PIGS.Actions.roll(1);
+	});
+    
+    $("#reset").click(function(){
+		location.reload();
 	});
 });
